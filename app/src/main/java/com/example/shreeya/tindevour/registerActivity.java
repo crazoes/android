9327d1fd281dde4fd189d95446798fa247d86818
Shreeya Patel <shreeya.patel23498@gmail.com>
2018-09-16 16:54:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shreeya/tindevour/registerActivity.java
@@ -0,0 +1,13 @@
+package com.example.shreeya.tindevour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
