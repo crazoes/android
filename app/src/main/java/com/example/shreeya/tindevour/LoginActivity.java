1b557d2810273420cb4bfe1f5985dcbe61e9dd90
Shreeya Patel <shreeya.patel23498@gmail.com>
2018-09-20 01:17:40 +0530

    Login Layout

---
+++ app/src/main/java/com/example/shreeya/tindevour/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.shreeya.tindevour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
