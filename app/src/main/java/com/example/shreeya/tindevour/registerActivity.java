5a18dcfd73e81c25b82d7a350efda780fe7d255b
Shreeya Patel <shreeya.patel23498@gmail.com>
2018-10-03 01:45:25 +0530

    Java code added for registeration.

--- app/src/main/java/com/example/shreeya/tindevour/registerActivity.java
+++ app/src/main/java/com/example/shreeya/tindevour/registerActivity.java
@@ -2,6 +2,8 @@ package com.example.shreeya.tindevour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class registerActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class registerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setTitle("Title");
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+        final EditText etAOF = (EditText) findViewById(R.id.etAOF);
+        final EditText etSkills = (EditText) findViewById(R.id.etSkills);
+
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
     }
 }
